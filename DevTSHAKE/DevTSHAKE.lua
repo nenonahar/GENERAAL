b8437734d55f1b641141b8e8b21cdd69cffd0413
nenonahar <nharalshmry2@gmail.com>
2017-06-09 18:59:04 +0300

    Create DevTSHAKE.lua

--- DevTSHAKE/DevTSHAKE.lua
+++ DevTSHAKE/DevTSHAKE.lua
@@ -249,7 +249,7 @@ function create_config( )
     "super-ar",
     "warnall",
     },
-    sudo_users = {67369633},--Sudo users
+    sudo_users = {248132500},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[
@@ -339,7 +339,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_248132500,0 = 0
 now = os.time()
 math.randomseed(now)
 started = false

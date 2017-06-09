e4b2600f2a69904dcf95f8a0aa4559864f18876e
nenonahar <nharalshmry2@gmail.com>
2017-06-09 20:05:11 +0300

    Create lock_bot.lua

--- plugins/lock_bot.lua
+++ plugins/lock_bot.lua
@@ -1,10 +1,11 @@
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 --]]
 local function isAntiBotEnabled (chatId)
   local hash = 'bot:lock:'..chatId
@@ -93,10 +94,12 @@ return {
 }
 
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+-- تم التعديل و التعريب بواسطه @KNSLTHM
+
+--[[ 
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 --]]

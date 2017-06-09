d801dbd59199852af700a89795630927c2f386aa
nenonahar <nharalshmry2@gmail.com>
2017-06-09 20:03:35 +0300

    Create leave_ban.lua

--- plugins/leave_ban.lua
+++ plugins/leave_ban.lua
@@ -1,10 +1,11 @@
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CHCH
 --]]
 local function run(msg, matches)
 	local data = load_data(_config.moderation.data)
@@ -32,12 +33,12 @@ return {
   },
   run = run
 }
+-- تم التعديل و التعريب بواسطه @KNSLTHM
 
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 --]]

95d46893fc852a5af6694dc10cf8e72e904738c1
nenonahar <nharalshmry2@gmail.com>
2017-06-10 18:10:18 +0300

    Create robot.lua

--- plugins/robot.lua
+++ plugins/robot.lua
@@ -1,10 +1,11 @@
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 --]]
 local function is_channel_disabled( receiver )
 	if not _config.disabled_channels then
@@ -91,11 +92,12 @@ return {
 	pre_process = pre_process
 }
 
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 --]]

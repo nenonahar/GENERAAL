84314f04399b566a9af108588361d704122100ad
nenonahar <nharalshmry2@gmail.com>
2017-06-10 18:15:48 +0300

    Create super-ar.lua

--- plugins/super-ar.lua
+++ plugins/super-ar.lua
@@ -1,10 +1,11 @@
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 سوبر الزعيم 
 تعديل المطورين الفووك :)
 --]]
@@ -2557,13 +2558,14 @@ return {
   pre_process = pre_process
 }
 --End supergrpup.lua
---By @TH3BOSS
+--By @KNSLTHM
+
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
 
 --[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-               CH > @lTSHAKEl_CH
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
 --]]
